ebf4bd91f784fef4c641b323610dcd2c15b894f5
e <alexandra.aldea2004@gmail.com>
2025-05-21 21:26:04 +0300

    fixed the required fields for sign up

--- Inpot/controllers/loginController.js
+++ Inpot/controllers/loginController.js
@@ -5,8 +5,17 @@ const loginController = {
         const formTitle = document.getElementById('form-title');
         const loginButton = document.getElementById('login');
         const formLabel = document.getElementById('form-label');
+        const authForm = document.getElementById('auth-form');
+
+        const firstName = document.getElementById('first-name');
+        const lastName = document.getElementById('last-name');
 
         let isSignup = false;
+
+        firstName.setAttribute('disabled', 'true');
+        lastName.setAttribute('disabled', 'true');
+        firstName.removeAttribute('required');
+        lastName.removeAttribute('required');
 
         toggleLink.addEventListener('click', (e) => {
             e.preventDefault();
@@ -16,18 +25,20 @@ const loginController = {
             formTitle.textContent = isSignup ? 'Register' : 'Login';
             loginButton.value = isSignup ? 'Register' : 'Login';
             formLabel.textContent = isSignup ? 'Fill in all fields to create an account' : 'Enter your email and password';
-            toggleLink.innerHTML = isSignup ? 'Already have an account? <br><strong> Login now!</strong>' : "Don't have an account? <br><strong> Register now! </strong>";
+            toggleLink.innerHTML = isSignup
+                ? 'Already have an account? <br><strong> Login now!</strong>'
+                : "Don't have an account? <br><strong> Register now! </strong>";
 
-           
-            document.getElementById('first-name').required = isSignup;
-            document.getElementById('last-name').required = isSignup;
-        });
-
-        
-        document.getElementById('auth-form').addEventListener('submit', function(e) {
-            if (!isSignup) {
-                document.getElementById('first-name').required = false;
-                document.getElementById('last-name').required = false;
+            if (isSignup) {
+                firstName.removeAttribute('disabled');
+                lastName.removeAttribute('disabled');
+                firstName.setAttribute('required', 'required');
+                lastName.setAttribute('required', 'required');
+            } else {
+                firstName.removeAttribute('required');
+                lastName.removeAttribute('required');
+                firstName.setAttribute('disabled', 'true');
+                lastName.setAttribute('disabled', 'true');
             }
         });
     }
